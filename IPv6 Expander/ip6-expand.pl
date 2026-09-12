65fee7fcbd2b38bb8b200fd42637c69dc9b085c6
Aruna Hewapathirane <aruna.hewapathirane@gmail.com>
2026-09-12 04:44:17 -0400

    Create ip6-expand.pl

---
+++ IPv6 Expander/ip6-expand.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/env perl
+use feature qw(say);
+use Socket  qw(inet_pton AF_INET6);
+
+@ip6s = split /\n/, `ip a | grep inet6`;
+
+for (@ip6s) {
+    s/^\s+//;
+    $addr = (split)[1];
+    $addr = substr $addr, 0, index $addr, "/";
+    $addr = join ":", unpack "H4" x 8, inet_pton AF_INET6, $addr;
+    say $addr;
+}
